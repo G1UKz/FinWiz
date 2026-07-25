b4a8636b4efd5248a7c7e80686eee1a446035b1b
Киселев Иван Дмитриевич <IKiselev@nota.tech>
2026-07-26 02:43:35 +0300

    chore: add VSCode settings, DevTools, debug logging

--- src/main/java/finwiz/app/service/BankAccountService.java
+++ src/main/java/finwiz/app/service/BankAccountService.java
@@ -73,7 +73,6 @@ public class BankAccountService {
                 account.getBalance(),
                 account.getIsActive(),
                 account.getNote(),
-                account.getCreatedAt()
-        );
+                account.getCreatedAt());
     }
 }
